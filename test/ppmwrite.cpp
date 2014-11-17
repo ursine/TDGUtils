6e90606cae1c0a81669fc54b8c4b031960c971e3
Gary Coulbourne <bear@bears.org>
2014-11-17 07:21:23 -0500

    draw blue line

--- test/ppmwrite.cpp
+++ test/ppmwrite.cpp
@@ -9,12 +9,14 @@ class Image
 {
 public:
   using csize = uint8_t;
+  constexpr static size_t bsize = sizeof(csize) * 3;
   
   Image() :
     Image(320,200) {}
 
   Image(const uintmax_t w, const uintmax_t h) :
-    image(w * h * 3 * sizeof(csize)),
+    image(w * h * bsize),
+    _byteWidth(w * bsize),
     _width(w),
     _height(h) {}
    
@@ -41,9 +43,28 @@ public:
 	    image[i+2] = b;
       }
   }
+
+  void color(const uint8_t r, const uint8_t g, const uint8_t b)
+  {
+    red = r; green = g; blue = b; 
+  }
+  
+  void set(const uintmax_t x, const uintmax_t y)
+  {
+    const size_t tbyte = (y * _byteWidth) + (bsize * x);
+    image[tbyte + 0] = red;
+    image[tbyte + 1] = green;
+    image[tbyte + 2] = blue;
+  }
   
 private:
+  uint8_t red;
+  uint8_t blue;
+  uint8_t green;
+  
   std::vector<uint8_t> image;
+
+  size_t    _byteWidth;
   uintmax_t _width;
   uintmax_t _height;
 };
@@ -52,6 +73,11 @@ int main()
 {
   Image i;
   i.fill(255,0,0);
+
+  i.color(0,0,255);
+
+  for(int x=0; x < 200; x++) { i.set(x,x); }
+  
   i.writeppm("testfile.ppm");
 }
 
